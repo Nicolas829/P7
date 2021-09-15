6cb6a82049a60c87d6a7a4e7dda4284879daebfd
Nicolas Lebordais <nico.lebordais@gmail.com>
2021-09-15 10:39:32 +0200

    dropdown + search placeholder en cours

---
+++ js/algo.js
@@ -0,0 +1,25 @@
+import { getData } from "./api.js";
+import { createCard } from "./createCard.js";
+import {variables} from "./variables.js";
+import {dropDownList} from "./dropdown.js";
+
+
+let placeHolder=document.getElementById("search")
+let ulIngredient = document.getElementById('dropdown-ingredients')
+
+
+
+variables()
+
+function searchIngredient() {
+
+    
+
+ 
+    
+  
+
+
+}
+
+export {searchIngredient};
