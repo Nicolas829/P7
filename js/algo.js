9e5cdc4cd1970bd361e10f708949d2097d3df9e3
nicolas <nico.lebordais@gmail.com>
2021-10-22 08:35:22 +0200

    algo 1

--- js/algo.js
+++ js/algo.js
@@ -2,39 +2,16 @@ import { getData} from "./api.js";
 
 let data=await getData()
 
-let liste=[]
-
-data.forEach(element=>liste.push(element))
+console.log(data)
 
 
-class listeFactory{
-    constructor (liste){
-        this.liste=liste      
-        liste.forEach(element => new nameFactory(element.name))
-       
-        return liste
-
-    }
-}
 
 
-class nameFactory {
-    constructor (element){
-        this.element=element
-        console.log(element)
-        const listeIngredients=[]
-        listeIngredients.push(element)
-        console.log(listeIngredients)
-    }
 
 
-}
-
-const ingredients=new nameFactory(liste)
-console.log(ingredients)
 
 
-export {listeFactory} ;
+
 
 
 
--- js/dropdown.js
+++ js/dropdown.js
@@ -1,23 +1,67 @@
 import { getData } from "./api.js";
 
 //DOM
-let ingredients=document.getElementById("btn_ingredients")
-let liste=[]
-let listeIngredients=[]
-let listeIngre=[]
+let dropDownIngredients=document.getElementById("dropdown-ingredients")
+let dropDownUstensils=document.getElementById("ustensils")
+let dropDownAppliance= document.getElementById("appareils")
+
 let data=await getData()
 
-data.forEach(element=>liste.push(element))
+let name = data.map(element=>element.name)
 
+//on crée le tableau avec les ingrédients
 
+function getIngredients (ingredient){
+ let ingredients =data.map(element=>element.ingredients)
+ ingredient=[]
+ingredients.forEach(element=>{for (let i=0;i<element.length;i++){(ingredient.push(element[i].ingredient))}})
 
-//on remplit les dropDown
-function dropDownFill (){
-  listeIngredients= liste.map(element=>element.ingredients)
-  console.log(listeIngredients)
-
-
+return ingredient
 }
-dropDownFill()
+
+
+//on trie les élements
+let ingredient = getIngredients()
+ingredient.sort()
+ingredient=[...new Set(ingredient)]
+
+let ustensils=data.map(element=>element.ustensils)
+console.log(ustensils)
+
+function getUstensils (ustensils){
+    ustensils =data.map(element=>element.ustensils)
+   let ustensil=[]
+
+   console.log(ustensils.ustensils)
+   ustensils.forEach(element=>{for (let i=0;i<element.length;i++){(ustensil.push(element[i]))}})
+   
+   return ustensil
+   }
+//on crée le tableau d'appareils
+let appliance=data.map(element=>element.appliance)
+appliance.sort()
+appliance=[...new Set(appliance)]
+console.log(appliance)
+
+let ustensil=getUstensils(ustensils)
+ustensil.sort()
+ustensil=[...new Set(ustensil)]
+
+
+//on crée la fonction de remplissage des dropdown
+function dropDownFill(elements, target){
+    elements.forEach(element=>{
+    let li=document.createElement("li")
+    target.appendChild(li)
+    li.innerText=element})
+}
+
+
+
+
+dropDownFill(appliance, dropDownAppliance)
+dropDownFill(ingredient, dropDownIngredients)
+dropDownFill(ustensil, dropDownUstensils)
+
 
 export{dropDownFill}
