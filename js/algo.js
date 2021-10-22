781e5b3d2de0150387196f74124313be1d43b373
nicolas <nico.lebordais@gmail.com>
2021-10-22 12:23:05 +0200

    algo1

--- js/algo.js
+++ js/algo.js
@@ -1,17 +1,20 @@
 import { getData} from "./api.js";
+//DOM
+let inputSearch=document.getElementById("search")
+
 
 let data=await getData()
 
-console.log(data)
+
 //binarysearch test
-function search (data, index, column, element) {
+function showCardSearch (data, index, column, element) {
 
     let first=0
     let last = data.length -1
     while(first<=last){
         let middle = Math.floor((first+last)/2)
         console.log(data[middle][column])
-        if(data[middle][column]<index){
+        if(data[middle][column] <index){
             first = middle+1            
         }
         else if(data[middle][column]>index) {
@@ -24,10 +27,28 @@ function search (data, index, column, element) {
     }
     return false
 }
+let result=[]
+function search(result){
+inputSearch.addEventListener("input",(event, result)=> {
+    
+if(event.target.value.length>2) {
+    result=[]
+    result.push(event.target.value)
+    if (data.includes(result)){
+        
+    }
+    return result
+}}
+)}
 
-console.log(search(data,10, "id", "ingredients"))
 
-//test 
+
+
+
+
+
+
+
 
 
 
